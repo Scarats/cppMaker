b8287e63d213c60cabffb50e42758b3fde8e0134
Thomas.42 <0xscarats@gmail.com>
2026-02-09 14:57:22 +0100

    WIP

--- fileMakers/addToMakefile.cpp
+++ fileMakers/addToMakefile.cpp
@@ -1,6 +1,6 @@
 #include "../cppMaker.main.hpp"
 
-bool checkFile(std::string &filepath)
+bool checkFile(const std::string &filepath)
 {
 	if (std::filesystem::exists(filepath))
 		return (false);
@@ -10,28 +10,43 @@ bool checkFile(std::string &filepath)
 bool addToMakefile(MenuItem *item, MenuList *menu)
 {
     if (!handleInputYesNo(menu, "Add .cpp to Makefile", MenuField::Name))
-        return (false);
+        return false;
 
-    std::string path = "Makefile";
-    if (!checkFile(path)) 
+    std::string const path = "Makefile";
+    if (!checkFile(path))
     {
         item->setColor(RED);
-        return (false);
+        return false;
     }
-    std::ifstream in("Makefile");
-    std::ofstream out("Makefile");
-    std::string fileName = BASIC_PATH_CPP + item->getInput().getFileName();
+
+    std::ifstream in(path);
+    std::ofstream out("Makefile.tmp");
+
+    if (!in || !out)
+        return false;
+
+    std::string fileName = BASIC_PATH_CPP + item->getInput().getFileName() + " ";
+    const std::string pattern = "SRC = ";
 
     std::string line;
-    const std::string pattern = "SRC = ";
+    bool inserted = false;
 
     while (std::getline(in, line))
     {
         std::size_t pos = line.find(pattern);
-        if (pos != std::string::npos) {
-            // insert right after the pattern
+        if (pos != std::string::npos && !inserted)
+        {
             line.insert(pos + pattern.length(), fileName);
+            inserted = true;
         }
+        out << line << '\n';
     }
-    return (true);
+
+    in.close();
+    out.close();
+
+    std::remove("Makefile");
+    std::rename("Makefile.tmp", "Makefile");
+
+    return true;
 }
